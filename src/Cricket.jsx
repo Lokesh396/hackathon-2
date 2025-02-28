9ec76be9bb09655862a5d75d59da7041c715b849
lokeshkr5 <lokesh.c@way2news.com>
2025-02-28 17:42:47 +0530

    removed unnencessary return string in Cricket page

--- src/Cricket.jsx
+++ src/Cricket.jsx
@@ -50,7 +50,6 @@ function Birthday() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 space-y-6 w-full">
-        return (
     <Confetti
       width={width}
       height={height}
